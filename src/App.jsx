a63c92494dfeb45390beabfa8fa45e4b0258b4f8
alaminrony <alamin.onest@gmail.com>
2023-05-25 23:56:25 +0600

    inline style + external css done

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ function App() {
   return (
     <>
       <div>
-        <h1>Hello World</h1>
         </div>
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,24 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+const myName = 'Alamin Rony';
+const date = new Date();
+const day = date.getDate();
+const month = date.getMonth();
+const year = date.getFullYear();
+
+// const headingStyle = {
+//   backgroundColor : "purple",
+//   color : "white",
+//   textAlign : "center",
+//   padding :"10px"
+// }
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ 
+    <div>
+      <h1 className="headingStyle">Hello World</h1>
+      <p>{myName}</p>
+      <p>{`day is ${day} Month is ${month} year is ${year}`}</p>
+    </div>
+ 
 )
